ce76db910b93738bff29967d74be3cac6f711d74
Huriel <huri3l.lopes@gmail.com>
2021-11-25 20:41:00 -0300

    JSON Server

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Post } from '../Post';
-import { posts } from '../../posts.json';
+import { api } from '../../services/api';
 
 import './styles.css';
 
 export function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    api.get('/posts').then((response) => {
+      console.log(response);
+      setPosts(response.data);
+    });
+  }, []);
+
   return (
     <ul>
-      {posts.map((post) => {
+      {posts?.map((post) => {
         return <Post title={post.title} description={post.description} />;
       })}
     </ul>
